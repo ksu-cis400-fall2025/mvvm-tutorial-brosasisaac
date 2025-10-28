db0f7771b6d757a22c47e4c0819f06ffdedf6dc6
Isaac Brosas <brosasisaac@ksu.edu>
2025-10-27 22:52:27 -0500

    done

---
+++ MvvmExample/ComputerScienceStudentViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmExample
+{
+    public class ComputerScienceStudentViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Student
+        /// </summary>
+        private Student CSStudent { get; init; }
+
+        public string FirstName => CSStudent.FirstName;
+
+        public string LastName => CSStudent.LastName;
+
+        public double GPA => CSStudent. GPA;
+
+        public IEnumerable<CourseRecord> CourseRecords => CSStudent.CourseRecords; 
+
+        public double CSGPA
+        {
+            get
+            {
+                var points = 0.0;
+                var hours = 0.0;
+                foreach (var cr in CSStudent.CourseRecords)
+                {
+                    if (cr.CourseName.Contains("CIS"))
+                    {
+                        points += (double)cr.Grade * cr.CreditHours;
+                        hours += cr.CreditHours;
+                    }
+                }
+                return points / hours;
+            }
+        }
+
+        private void HandleStudentPropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(Student.GPA))
+            {
+                PropertyChanged?.Invoke(this, e);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CSGPA)));
+            }
+        }
+
+       public ComputerScienceStudentViewModel(Student student)
+        {
+            CSStudent = student;
+            student.PropertyChanged += HandleStudentPropertyChanged;
+        }
+
+    }
+}
